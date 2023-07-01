caad282cdbf08d9f677d697b6846e9761641f6ff
Bob <rfobrien76@gmail.com>
2023-07-01 08:44:33 -0400

    fixed signout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import { Amplify } from 'aws-amplify';
-import awsConfig from './aws-exports';
+import awsExports from './aws-exports';
+import {Authenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsConfig);
 
-function App() {
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Family Helper Amplify</h1>
-    </div>
+    <Authenticator>
+      
+        {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      
+    </Authenticator>
   );
 }
 
-export default App;
+//export default App;
